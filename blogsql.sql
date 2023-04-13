b1fb7332f036183946c51e3a9ab22fdebc7ff35b
y <623573553@qq.com>
2023-04-13 14:50:01 +0800

    修改sql文件

--- blogsql.sql
+++ blogsql.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80022
  File Encoding         : 65001
 
- Date: 12/04/2023 15:34:39
+ Date: 13/04/2023 14:49:12
 */
 
 SET NAMES utf8mb4;
@@ -28,7 +28,7 @@ CREATE TABLE `advertisement`  (
   `url` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NULL DEFAULT NULL COMMENT '广告链接',
   `remark` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NULL DEFAULT NULL,
   `createTime` datetime(0) NULL DEFAULT NULL,
-  `isDel` tinyint(1) NULL DEFAULT 0,
+  `isDel` tinyint(1) NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Dynamic;
 
@@ -46,7 +46,7 @@ CREATE TABLE `article`  (
   `lookCount` int(0) NULL DEFAULT NULL COMMENT '文章访问次数',
   `likeCount` int(0) NULL DEFAULT NULL COMMENT '点赞数',
   `commentCount` int(0) NULL DEFAULT NULL COMMENT '评论数',
-  `isDel` tinyint(1) NULL DEFAULT 0,
+  `isDel` tinyint(1) NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Dynamic;
 
@@ -77,7 +77,7 @@ CREATE TABLE `comments`  (
   `comment` varchar(1200) CHARACTER SET utf8 COLLATE utf8_bin NULL DEFAULT NULL COMMENT '评论，500字以内',
   `createTime` datetime(0) NULL DEFAULT NULL,
   `createName` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NULL DEFAULT '游客' COMMENT '可以为空，默认为游客访问',
-  `isDel` tinyint(1) NULL DEFAULT 0,
+  `isDel` tinyint(1) NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Dynamic;
 
@@ -106,19 +106,21 @@ CREATE TABLE `exceptionlog`  (
 DROP TABLE IF EXISTS `menus`;
 CREATE TABLE `menus`  (
   `id` bigint(0) NOT NULL AUTO_INCREMENT,
-  `menuNames` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NULL DEFAULT NULL,
+  `menuNames` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   `parentId` int(0) NULL DEFAULT NULL COMMENT 'parentId 为0说明是根节点',
   `depth` int(0) NULL DEFAULT NULL COMMENT '根节点下面的最大子节点的长度',
-  `url` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NULL DEFAULT NULL COMMENT '对应的路由',
+  `url` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL COMMENT '对应的路由',
+  `isDel` tinyint(1) NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 6 CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 7 CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of menus
 -- ----------------------------
-INSERT INTO `menus` VALUES (1, '用户管理', 0, NULL, '/user');
-INSERT INTO `menus` VALUES (2, '角色管理', 0, NULL, '/role');
-INSERT INTO `menus` VALUES (3, '权限管理', 0, NULL, '/permisson');
+INSERT INTO `menus` VALUES (1, '用户管理', 0, NULL, '/user', 0);
+INSERT INTO `menus` VALUES (2, '角色管理', 0, NULL, '/role', 0);
+INSERT INTO `menus` VALUES (3, '权限管理', 0, NULL, '/permisson', 0);
+INSERT INTO `menus` VALUES (6, '主界面', 0, NULL, '/home', 0);
 
 -- ----------------------------
 -- Table structure for operatelog
@@ -132,7 +134,7 @@ CREATE TABLE `operatelog`  (
   `ip` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NULL DEFAULT NULL COMMENT '来访的ip',
   `logTime` datetime(0) NULL DEFAULT NULL COMMENT '访问的时间',
   `username` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NULL DEFAULT NULL COMMENT '可以为空，如果是没登录的用户',
-  `isDel` tinyint(1) NULL DEFAULT 0 COMMENT '0:未删除，1:已删除',
+  `isDel` tinyint(1) NOT NULL DEFAULT 0 COMMENT '0:未删除，1:已删除',
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Dynamic;
 
@@ -144,18 +146,18 @@ CREATE TABLE `permission`  (
   `id` bigint(0) NOT NULL AUTO_INCREMENT,
   `icon` varchar(1000) CHARACTER SET utf8 COLLATE utf8_bin NULL DEFAULT NULL COMMENT '菜单图标',
   `isBtn` tinyint(1) NULL DEFAULT 0 COMMENT '是否显示菜单为按钮，0代表不显示，1显示',
-  `isShow` tinyint(1) NULL DEFAULT 0 COMMENT '是否显示该菜单项，0代表不显示，1代表显示',
+  `isShow` tinyint(1) NOT NULL DEFAULT 0 COMMENT '是否显示该菜单项，0代表不显示，1代表显示',
   `description` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NULL DEFAULT NULL,
-  `enable` tinyint(1) NULL DEFAULT 1 COMMENT '是否启用，1代表启用，0代表不启用',
-  `isDel` tinyint(1) NULL DEFAULT 0 COMMENT '是否逻辑删除，0代表未删除，1代表删除\r\n是否逻辑删除，0代表未删除，1代表删除',
+  `enable` tinyint(1) NOT NULL DEFAULT 1 COMMENT '是否启用，1代表启用，0代表不启用',
+  `isDel` tinyint(1) NOT NULL DEFAULT 0 COMMENT '是否逻辑删除，0代表未删除，1代表删除\r\n是否逻辑删除，0代表未删除，1代表删除',
   `createName` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NULL DEFAULT NULL,
   `createTime` datetime(0) NULL DEFAULT NULL,
   `modifyName` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NULL DEFAULT NULL COMMENT '修改人的用户名',
   `modifyTime` datetime(0) NULL DEFAULT NULL COMMENT '修改时间',
-  `menuId` bigint(0) NULL DEFAULT NULL COMMENT '菜单id',
+  `menuId` bigint(0) NOT NULL COMMENT '菜单id',
   PRIMARY KEY (`id`) USING BTREE,
   INDEX `menuIdForKey`(`menuId`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 6 CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of permission
@@ -163,6 +165,7 @@ CREATE TABLE `permission`  (
 INSERT INTO `permission` VALUES (2, NULL, 0, 1, NULL, 1, 0, 'admin', '2023-04-04 17:29:51', NULL, NULL, 1);
 INSERT INTO `permission` VALUES (3, NULL, 0, 0, NULL, 1, 0, 'admin', '2023-04-04 17:32:00', NULL, NULL, 2);
 INSERT INTO `permission` VALUES (4, NULL, 0, 0, NULL, 1, 0, 'admin', '2023-04-04 17:32:16', NULL, NULL, 3);
+INSERT INTO `permission` VALUES (5, NULL, 0, 0, NULL, 1, 0, 'admin', '2023-04-13 13:44:52', NULL, NULL, 6);
 
 -- ----------------------------
 -- Table structure for role
@@ -173,18 +176,19 @@ CREATE TABLE `role`  (
   `roleName` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL COMMENT '角色名',
   `description` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NULL DEFAULT NULL COMMENT '角色描述',
   `level` int(0) NULL DEFAULT NULL COMMENT '角色等级',
-  `enable` tinyint(1) NULL DEFAULT 1 COMMENT '是否启用，1代表启用，0代表不启用',
-  `isDel` tinyint(1) NULL DEFAULT 0 COMMENT '是否逻辑删除，0代表未删除，1代表删除',
+  `enable` tinyint(1) NOT NULL DEFAULT 1 COMMENT '是否启用，1代表启用，0代表不启用',
+  `isDel` tinyint(1) NOT NULL DEFAULT 0 COMMENT '是否逻辑删除，0代表未删除，1代表删除',
   `createUsername` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL COMMENT '创建的用户名',
   `createTime` datetime(0) NOT NULL COMMENT '创建时间',
   `modifyTime` datetime(0) NULL DEFAULT NULL COMMENT '修改时间',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of role
 -- ----------------------------
-INSERT INTO `role` VALUES (1, '管理员', NULL, NULL, 1, 0, '管理员', '2023-04-04 17:05:16', NULL);
+INSERT INTO `role` VALUES (1, 'admin', '管理员', NULL, 1, 0, '管理员', '2023-04-04 17:05:16', NULL);
+INSERT INTO `role` VALUES (2, 'user', '普通用户', NULL, 1, 0, '管理员', '2023-04-12 15:43:33', NULL);
 
 -- ----------------------------
 -- Table structure for rolepermission
@@ -194,8 +198,8 @@ CREATE TABLE `rolepermission`  (
   `id` bigint(0) NOT NULL AUTO_INCREMENT,
   `roleId` bigint(0) NOT NULL COMMENT '外键，角色表',
   `permissionId` bigint(0) NOT NULL COMMENT '外键，permission表',
-  `isDel` tinyint(1) NULL DEFAULT 0 COMMENT '是否逻辑删除，0代表未删除，1代表删除',
-  `enabel` tinyint(1) NULL DEFAULT 1 COMMENT '是否启用，1代表启用，0代表不启用',
+  `isDel` tinyint(1) NOT NULL DEFAULT 0 COMMENT '是否逻辑删除，0代表未删除，1代表删除',
+  `enabel` tinyint(1) NOT NULL DEFAULT 1 COMMENT '是否启用，1代表启用，0代表不启用',
   `createUsername` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NULL DEFAULT NULL,
   `createTime` datetime(0) NULL DEFAULT NULL,
   `modifyUsername` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NULL DEFAULT NULL,
@@ -203,7 +207,7 @@ CREATE TABLE `rolepermission`  (
   PRIMARY KEY (`id`) USING BTREE,
   INDEX `roleForKey`(`roleId`) USING BTREE,
   INDEX `perForKey`(`permissionId`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of rolepermission
@@ -211,6 +215,7 @@ CREATE TABLE `rolepermission`  (
 INSERT INTO `rolepermission` VALUES (1, 1, 2, 0, 1, 'admin', '2023-04-04 17:34:01', NULL, NULL);
 INSERT INTO `rolepermission` VALUES (2, 1, 3, 0, 1, 'admin', '2023-04-04 17:34:19', NULL, NULL);
 INSERT INTO `rolepermission` VALUES (3, 1, 4, 0, 1, 'admin', '2023-04-04 17:34:39', NULL, NULL);
+INSERT INTO `rolepermission` VALUES (4, 2, 5, 0, 1, 'admin', '2023-04-13 13:45:14', NULL, NULL);
 
 -- ----------------------------
 -- Table structure for user
@@ -228,16 +233,17 @@ CREATE TABLE `user`  (
   `sex` int(0) NULL DEFAULT NULL COMMENT '性别 0:男  1女',
   `age` int(0) NULL DEFAULT NULL COMMENT '年龄',
   `birth` datetime(6) NULL DEFAULT NULL COMMENT '生日',
-  `isDel` tinyint(1) NULL DEFAULT 0 COMMENT '是否逻辑删除，0代表未删除，1代表删除',
-  `isEnable` tinyint(1) NULL DEFAULT 1 COMMENT '是否启用，1代表启用，0代表不启用',
+  `isDel` tinyint(1) NOT NULL DEFAULT 0 COMMENT '是否逻辑删除，0代表未删除，1代表删除',
+  `isEnable` tinyint(1) NOT NULL DEFAULT 1 COMMENT '是否启用，1代表启用，0代表不启用',
   `modifyName` varchar(25) CHARACTER SET utf8 COLLATE utf8_bin NULL DEFAULT NULL COMMENT '修改信息用户名',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 7 CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of user
 -- ----------------------------
-INSERT INTO `user` VALUES (1, 'admin', 'admin', '2023-04-03 17:25:44', NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, 1, NULL);
+INSERT INTO `user` VALUES (5, 'yu', '202CB962AC59075B964B07152D234B70', '2023-04-13 09:38:30', NULL, NULL, NULL, NULL, 0, 0, '2023-04-13 00:00:00.000000', 0, 1, NULL);
+INSERT INTO `user` VALUES (6, 'admin', '21232F297A57A5A743894A0E4A801FC3', '2023-04-13 10:57:35', NULL, NULL, NULL, '123', 0, 0, '2023-04-13 00:00:00.000000', 0, 1, NULL);
 
 -- ----------------------------
 -- Table structure for userrole
@@ -247,8 +253,8 @@ CREATE TABLE `userrole`  (
   `id` bigint(0) NOT NULL AUTO_INCREMENT,
   `userId` bigint(0) NOT NULL COMMENT '外键，关联user表id',
   `roleId` bigint(0) NOT NULL COMMENT '外键，关联角色表id',
-  `isDel` tinyint(1) NULL DEFAULT 0 COMMENT '是否逻辑删除，0代表未删除，1代表删除',
-  `enable` tinyint(1) NULL DEFAULT 1 COMMENT '是否启用，1代表启用，0代表不启用',
+  `isDel` tinyint(1) NOT NULL DEFAULT 0 COMMENT '是否逻辑删除，0代表未删除，1代表删除',
+  `enable` tinyint(1) NOT NULL DEFAULT 1 COMMENT '是否启用，1代表启用，0代表不启用',
   `createUsername` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NULL DEFAULT NULL,
   `createTime` datetime(0) NULL DEFAULT NULL,
   `modifyUsername` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NULL DEFAULT NULL,
@@ -256,11 +262,13 @@ CREATE TABLE `userrole`  (
   PRIMARY KEY (`id`) USING BTREE,
   INDEX `useridForKey`(`userId`) USING BTREE,
   INDEX `roleidForKey`(`roleId`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 8 CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of userrole
 -- ----------------------------
-INSERT INTO `userrole` VALUES (1, 1, 1, 0, 1, 'admin', '2023-04-04 17:32:52', NULL, NULL);
+INSERT INTO `userrole` VALUES (5, 5, 1, 0, 1, NULL, '2023-04-13 10:19:26', NULL, NULL);
+INSERT INTO `userrole` VALUES (6, 6, 1, 0, 1, NULL, '2023-04-13 10:57:35', NULL, NULL);
+INSERT INTO `userrole` VALUES (7, 5, 2, 0, 1, NULL, '2023-04-13 11:09:18', NULL, NULL);
 
 SET FOREIGN_KEY_CHECKS = 1;
