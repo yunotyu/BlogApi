4e0155b5e192446c5beaf96dd64f8376c30324ab
y <623573553@qq.com>
2023-04-21 18:01:45 +0800

    修改sql文件

--- blogsql.sql
+++ blogsql.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80022
  File Encoding         : 65001
 
- Date: 19/04/2023 16:18:33
+ Date: 21/04/2023 18:01:12
 */
 
 SET NAMES utf8mb4;
@@ -30,7 +30,7 @@ CREATE TABLE `advertisement`  (
   `createTime` datetime(0) NULL DEFAULT NULL,
   `isDel` tinyint(1) NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for article
@@ -48,7 +48,7 @@ CREATE TABLE `article`  (
   `commentCount` int(0) NULL DEFAULT NULL COMMENT '评论数',
   `isDel` tinyint(1) NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of article
@@ -66,7 +66,7 @@ CREATE TABLE `collection`  (
   `createTime` datetime(0) NULL DEFAULT NULL,
   `isDel` tinyint(1) NULL DEFAULT 0,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for comments
@@ -80,7 +80,7 @@ CREATE TABLE `comments`  (
   `createName` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NULL DEFAULT '游客' COMMENT '可以为空，默认为游客访问',
   `isDel` tinyint(1) NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of comments
@@ -99,7 +99,7 @@ CREATE TABLE `exceptionlog`  (
   `message` varchar(2000) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL COMMENT '异常信息',
   `exceptionType` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL COMMENT '异常类型',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for menus
@@ -122,6 +122,10 @@ INSERT INTO `menus` VALUES (1, '用户管理', 0, NULL, '/user', 0);
 INSERT INTO `menus` VALUES (2, '角色管理', 0, NULL, '/role', 0);
 INSERT INTO `menus` VALUES (3, '权限管理', 0, NULL, '/permisson', 0);
 INSERT INTO `menus` VALUES (6, '主界面', 0, NULL, '/home', 0);
+INSERT INTO `menus` VALUES (7, '子用户管理1', 1, NULL, '/user1', 0);
+INSERT INTO `menus` VALUES (8, '子角色管理1', 2, NULL, '/role1', 0);
+INSERT INTO `menus` VALUES (9, '子用户管理11', 7, NULL, '/user11', 0);
+INSERT INTO `menus` VALUES (10, '子用户管理2', 1, NULL, '/user2', 0);
 
 -- ----------------------------
 -- Table structure for operatelog
@@ -137,7 +141,7 @@ CREATE TABLE `operatelog`  (
   `username` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NULL DEFAULT NULL COMMENT '可以为空，如果是没登录的用户',
   `isDel` tinyint(1) NOT NULL DEFAULT 0 COMMENT '0:未删除，1:已删除',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for permission
@@ -149,23 +153,44 @@ CREATE TABLE `permission`  (
   `isBtn` tinyint(1) NULL DEFAULT 0 COMMENT '是否显示菜单为按钮，0代表不显示，1显示',
   `isShow` tinyint(1) NOT NULL DEFAULT 0 COMMENT '是否显示该菜单项，0代表不显示，1代表显示',
   `description` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NULL DEFAULT NULL,
-  `isDel` tinyint(1) NOT NULL DEFAULT 0 COMMENT '是否逻辑删除，0代表未删除，1代表删除\r\n是否逻辑删除，0代表未删除，1代表删除',
   `createName` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NULL DEFAULT NULL,
   `createTime` datetime(0) NULL DEFAULT NULL,
   `modifyName` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NULL DEFAULT NULL COMMENT '修改人的用户名',
   `modifyTime` datetime(0) NULL DEFAULT NULL COMMENT '修改时间',
-  `menuId` bigint(0) NOT NULL COMMENT '菜单id',
-  PRIMARY KEY (`id`) USING BTREE,
-  INDEX `menuIdForKey`(`menuId`) USING BTREE
+  `menuId` bigint(0) NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 6 CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of permission
 -- ----------------------------
-INSERT INTO `permission` VALUES (2, NULL, 0, 1, NULL, 0, 'admin', '2023-04-04 17:29:51', NULL, NULL, 1);
-INSERT INTO `permission` VALUES (3, NULL, 0, 0, NULL, 0, 'admin', '2023-04-04 17:32:00', NULL, NULL, 2);
-INSERT INTO `permission` VALUES (4, NULL, 0, 0, NULL, 0, 'admin', '2023-04-04 17:32:16', NULL, NULL, 3);
-INSERT INTO `permission` VALUES (5, NULL, 0, 0, NULL, 0, 'admin', '2023-04-13 13:44:52', NULL, NULL, 6);
+INSERT INTO `permission` VALUES (2, 'xxxx', 1, 0, 'string', '', '2023-04-21 14:24:52', '', '2023-04-21 14:24:52', 1);
+INSERT INTO `permission` VALUES (3, 'ggg', 1, 0, 'string', '', '2023-04-21 14:24:52', '', '2023-04-21 14:24:52', 2);
+INSERT INTO `permission` VALUES (4, NULL, 0, 0, NULL, 'admin', '2023-04-04 17:32:16', NULL, NULL, 3);
+INSERT INTO `permission` VALUES (5, NULL, 0, 0, NULL, 'admin', '2023-04-13 13:44:52', NULL, NULL, 4);
+INSERT INTO `permission` VALUES (9, '/home.png', 0, 1, 'home', '', '2023-04-21 14:59:41', '', '2023-04-21 14:59:41', 2);
+INSERT INTO `permission` VALUES (10, '/home2.png', 0, 1, 'home', '', '2023-04-21 15:00:56', '', '2023-04-21 15:00:56', 2);
+INSERT INTO `permission` VALUES (11, '/menu.png', 0, 1, 'menu', '', '2023-04-21 15:00:56', '', '2023-04-21 15:00:56', 3);
+
+-- ----------------------------
+-- Table structure for permissonmenu
+-- ----------------------------
+DROP TABLE IF EXISTS `permissonmenu`;
+CREATE TABLE `permissonmenu`  (
+  `id` bigint(0) NOT NULL AUTO_INCREMENT,
+  `permissonId` bigint(0) NULL DEFAULT NULL,
+  `menuId` bigint(0) NULL DEFAULT NULL,
+  `modifyName` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NULL DEFAULT NULL,
+  `modifyTime` datetime(0) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of permissonmenu
+-- ----------------------------
+INSERT INTO `permissonmenu` VALUES (1, 2, 1, NULL, NULL);
+INSERT INTO `permissonmenu` VALUES (2, 2, 2, NULL, NULL);
+INSERT INTO `permissonmenu` VALUES (3, 2, 3, NULL, NULL);
 
 -- ----------------------------
 -- Table structure for role
@@ -181,14 +206,16 @@ CREATE TABLE `role`  (
   `createUsername` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL COMMENT '创建的用户名',
   `createTime` datetime(0) NOT NULL COMMENT '创建时间',
   `modifyTime` datetime(0) NULL DEFAULT NULL COMMENT '修改时间',
+  `modifyUsername` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 6 CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of role
 -- ----------------------------
-INSERT INTO `role` VALUES (1, 'admin', '管理员', NULL, 1, 0, '管理员', '2023-04-04 17:05:16', NULL);
-INSERT INTO `role` VALUES (2, 'user', '普通用户', NULL, 1, 0, '管理员', '2023-04-12 15:43:33', NULL);
+INSERT INTO `role` VALUES (1, 'admin', '管理员', NULL, 1, 0, '管理员', '2023-04-04 17:05:16', NULL, NULL);
+INSERT INTO `role` VALUES (2, 'user', '普通用户', NULL, 1, 0, '管理员', '2023-04-12 15:43:33', NULL, NULL);
+INSERT INTO `role` VALUES (3, 'gggg', 'vvvv', NULL, 1, 1, '', '2023-04-20 17:45:54', '2023-04-20 17:45:54', NULL);
 
 -- ----------------------------
 -- Table structure for rolepermission
@@ -236,7 +263,7 @@ CREATE TABLE `user`  (
   `isEnable` tinyint(1) NOT NULL DEFAULT 1 COMMENT '是否启用，1代表启用，0代表不启用',
   `modifyName` varchar(25) CHARACTER SET utf8 COLLATE utf8_bin NULL DEFAULT NULL COMMENT '修改信息用户名',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 23 CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 28 CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of user
@@ -253,10 +280,11 @@ INSERT INTO `user` VALUES (15, 'y7', '202CB962AC59075B964B07152D234B70', '2023-0
 INSERT INTO `user` VALUES (16, 'y8', '202CB962AC59075B964B07152D234B70', '2023-04-17 11:29:17', NULL, NULL, NULL, NULL, 0, 0, '2023-04-17 11:28:06.818000', 0, 1, NULL);
 INSERT INTO `user` VALUES (17, 'y9', '202CB962AC59075B964B07152D234B70', '2023-04-17 11:29:22', NULL, NULL, NULL, NULL, 0, 0, '2023-04-17 11:28:06.818000', 0, 1, NULL);
 INSERT INTO `user` VALUES (18, 'y11', '202CB962AC59075B964B07152D234B70', '2023-04-17 11:29:27', NULL, NULL, NULL, NULL, 0, 0, '2023-04-17 11:28:06.818000', 0, 1, NULL);
-INSERT INTO `user` VALUES (19, 'yu', '202CB962AC59075B964B07152D234B70', '0001-01-01 00:00:00', NULL, NULL, NULL, 'ggg', 1, 20, '2023-04-17 15:25:38.298000', 0, 1, 'yu');
-INSERT INTO `user` VALUES (20, 'yu', '202CB962AC59075B964B07152D234B70', '0001-01-01 00:00:00', NULL, NULL, NULL, 'ggg', 1, 20, '2023-04-17 15:25:38.298000', 0, 1, 'yu');
-INSERT INTO `user` VALUES (21, 'yu', '202CB962AC59075B964B07152D234B70', '0001-01-01 00:00:00', NULL, NULL, NULL, 'ggg', 1, 20, '2023-04-17 15:25:38.298000', 0, 1, '');
-INSERT INTO `user` VALUES (22, 'yu', '202CB962AC59075B964B07152D234B70', '0001-01-01 00:00:00', NULL, NULL, NULL, 'ggg', 1, 20, '2023-04-17 15:25:38.298000', 0, 1, '');
+INSERT INTO `user` VALUES (23, 'admidn', '550A141F12DE6341FBA65B0AD0433500', '2023-04-19 17:31:57', NULL, NULL, NULL, NULL, 0, 0, '2023-04-19 17:25:39.675000', 0, 1, '');
+INSERT INTO `user` VALUES (24, 'yue', '202CB962AC59075B964B07152D234B70', '2023-04-19 17:31:57', NULL, NULL, NULL, NULL, 0, 0, '2023-04-19 17:25:39.675000', 0, 0, '');
+INSERT INTO `user` VALUES (25, 'yur', 'B45CFFE084DD3D20D928BEE85E7B0F21', '2023-04-19 17:31:57', NULL, NULL, NULL, NULL, 0, 0, '2023-04-19 17:25:39.675000', 0, 0, '');
+INSERT INTO `user` VALUES (26, 'ff', 'F7177163C833DFF4B38FC8D2872F1EC6', '2023-04-20 10:05:44', NULL, NULL, NULL, NULL, 0, 0, '2023-04-20 10:05:22.941000', 0, 1, NULL);
+INSERT INTO `user` VALUES (27, 'string', 'B45CFFE084DD3D20D928BEE85E7B0F21', '2023-04-20 15:49:49', NULL, NULL, NULL, NULL, 0, 0, '2023-04-20 15:49:45.482000', 0, 1, NULL);
 
 -- ----------------------------
 -- Table structure for userrole
@@ -266,7 +294,6 @@ CREATE TABLE `userrole`  (
   `id` bigint(0) NOT NULL AUTO_INCREMENT,
   `userId` bigint(0) NOT NULL COMMENT '外键，关联user表id',
   `roleId` bigint(0) NOT NULL COMMENT '外键，关联角色表id',
-  `isDel` tinyint(1) NOT NULL DEFAULT 0 COMMENT '是否逻辑删除，0代表未删除，1代表删除',
   `createUsername` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NULL DEFAULT NULL,
   `createTime` datetime(0) NULL DEFAULT NULL,
   `modifyUsername` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NULL DEFAULT NULL,
@@ -274,44 +301,25 @@ CREATE TABLE `userrole`  (
   PRIMARY KEY (`id`) USING BTREE,
   INDEX `useridForKey`(`userId`) USING BTREE,
   INDEX `roleidForKey`(`roleId`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 18 CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 27 CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of userrole
 -- ----------------------------
-INSERT INTO `userrole` VALUES (5, 5, 1, 0, NULL, '2023-04-13 10:19:26', NULL, NULL);
-INSERT INTO `userrole` VALUES (6, 6, 1, 0, NULL, '2023-04-13 10:57:35', NULL, NULL);
-INSERT INTO `userrole` VALUES (7, 5, 2, 0, NULL, '2023-04-13 11:09:18', NULL, NULL);
-INSERT INTO `userrole` VALUES (8, 9, 2, 0, NULL, '2023-04-17 11:28:21', NULL, NULL);
-INSERT INTO `userrole` VALUES (9, 10, 2, 0, NULL, '2023-04-17 11:28:33', NULL, NULL);
-INSERT INTO `userrole` VALUES (10, 11, 2, 0, NULL, '2023-04-17 11:28:51', NULL, NULL);
-INSERT INTO `userrole` VALUES (11, 12, 2, 0, NULL, '2023-04-17 11:28:54', NULL, NULL);
-INSERT INTO `userrole` VALUES (12, 13, 2, 0, NULL, '2023-04-17 11:28:58', NULL, NULL);
-INSERT INTO `userrole` VALUES (13, 14, 2, 0, NULL, '2023-04-17 11:29:06', NULL, NULL);
-INSERT INTO `userrole` VALUES (14, 15, 2, 0, NULL, '2023-04-17 11:29:11', NULL, NULL);
-INSERT INTO `userrole` VALUES (15, 16, 2, 0, NULL, '2023-04-17 11:29:17', NULL, NULL);
-INSERT INTO `userrole` VALUES (16, 17, 2, 0, NULL, '2023-04-17 11:29:22', NULL, NULL);
-INSERT INTO `userrole` VALUES (17, 18, 2, 0, NULL, '2023-04-17 11:29:27', NULL, NULL);
-
--- ----------------------------
--- Table structure for usertemp5558ccc4
--- ----------------------------
-DROP TABLE IF EXISTS `usertemp5558ccc4`;
-CREATE TABLE `usertemp5558ccc4`  (
-  `id` bigint(0) NOT NULL DEFAULT 0,
-  `username` varchar(25) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
-  `pwd` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
-  `createTime` datetime(0) NOT NULL COMMENT '创建时间',
-  `updateTime` datetime(0) NULL DEFAULT NULL COMMENT '修改信息时间',
-  `lastErrorTime` datetime(0) NULL DEFAULT NULL COMMENT '上次密码错误时间',
-  `errorCount` int(0) NULL DEFAULT NULL COMMENT '密码错误次数',
-  `remark` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NULL DEFAULT NULL COMMENT '备注',
-  `sex` int(0) NULL DEFAULT NULL COMMENT '性别 0:男  1女',
-  `age` int(0) NULL DEFAULT NULL COMMENT '年龄',
-  `birth` datetime(6) NULL DEFAULT NULL COMMENT '生日',
-  `isDel` tinyint(1) NOT NULL DEFAULT 0 COMMENT '是否逻辑删除，0代表未删除，1代表删除',
-  `isEnable` tinyint(1) NOT NULL DEFAULT 1 COMMENT '是否启用，1代表启用，0代表不启用',
-  `modifyName` varchar(25) CHARACTER SET utf8 COLLATE utf8_bin NULL DEFAULT NULL COMMENT '修改信息用户名'
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Dynamic;
+INSERT INTO `userrole` VALUES (5, 5, 1, NULL, '2023-04-13 10:19:26', NULL, NULL);
+INSERT INTO `userrole` VALUES (7, 5, 2, NULL, '2023-04-13 11:09:18', NULL, NULL);
+INSERT INTO `userrole` VALUES (8, 9, 2, NULL, '2023-04-17 11:28:21', NULL, NULL);
+INSERT INTO `userrole` VALUES (9, 10, 2, NULL, '2023-04-17 11:28:33', NULL, NULL);
+INSERT INTO `userrole` VALUES (10, 11, 2, NULL, '2023-04-17 11:28:51', NULL, NULL);
+INSERT INTO `userrole` VALUES (11, 12, 2, NULL, '2023-04-17 11:28:54', NULL, NULL);
+INSERT INTO `userrole` VALUES (12, 13, 2, NULL, '2023-04-17 11:28:58', NULL, NULL);
+INSERT INTO `userrole` VALUES (13, 14, 2, NULL, '2023-04-17 11:29:06', NULL, NULL);
+INSERT INTO `userrole` VALUES (14, 15, 2, NULL, '2023-04-17 11:29:11', NULL, NULL);
+INSERT INTO `userrole` VALUES (15, 16, 2, NULL, '2023-04-17 11:29:17', NULL, NULL);
+INSERT INTO `userrole` VALUES (16, 17, 2, NULL, '2023-04-17 11:29:22', NULL, NULL);
+INSERT INTO `userrole` VALUES (17, 18, 2, NULL, '2023-04-17 11:29:27', NULL, NULL);
+INSERT INTO `userrole` VALUES (18, 26, 2, NULL, '2023-04-20 10:05:45', NULL, NULL);
+INSERT INTO `userrole` VALUES (24, 27, 2, NULL, '2023-04-20 15:49:50', NULL, NULL);
+INSERT INTO `userrole` VALUES (25, 6, 1, '', '2023-04-20 15:59:08', '', '2023-04-20 15:59:08');
 
 SET FOREIGN_KEY_CHECKS = 1;
